898f2e9ff47a3473746fda9c6cfe7460a2081f62
Vikas Singh <vvikas23@gmail.com>
2017-02-03 14:54:37 +0000

    my first commit
    checking

---
+++ myfirstpod/myfirstpod/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myfirstpod
+//
+//  Created by Vikas Singh on 03/02/2017.
+//  Copyright © 2017 Vikas Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
